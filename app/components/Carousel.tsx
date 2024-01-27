54dae30764a46f88a6b1b3573b1089792f14c0ed
ooleksander <d2345e678n9@yandex.ru>
2024-01-27 11:57:16 +0500

    added carousel and prototype of block

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,333 +1,171 @@
+"use client";
 import cx from "classnames";
 import styles from "./Carousel.module.css";
 import gsap from "gsap";
 import { useEffect, useRef, useState } from "react";
 import { TweenMax, Expo, Quint } from "gsap"; // Assuming you have GSAP installed
-import $ from 'jquery';
+import ImageTest from "../assets/images/heroDaria1.jpg";
+import Image from "next/image";
 
 export function Carousel() {
-  const contentContainer = useRef(null);
-  const carouselItem = useRef(null);
-  const carouselItemInner = useRef(null);
-  const carouselContainer = useRef(null)
+  const dragContainer = useRef(null);
+  const spinContainer = useRef(null);
+  const musicContainer = useRef(null);
+  const groundRef = useRef(null);
+  
 
-//   useEffect(() => {
-//     // set and cache variables
-// 		var w, container, carousel, item, radius, itemLength, rY, ticker, fps; 
-// 		var mouseX = 0;
-// 		var mouseY = 0;
-// 		var mouseZ = 0;
-// 		var addX = 0;
-		
-		
-// 		// fps counter created by: https://gist.github.com/sharkbrainguy/1156092,
-// 		// no need to create my own :)
-// 		var fps_counter = {
-			
-// 			tick: function () 
-// 			{
-// 				// this has to clone the array every tick so that
-// 				// separate instances won't share state 
-// 				this.times = this.times.concat(+new Date());
-// 				var seconds, times = this.times;
-        
-// 				if (times.length > this.span + 1) 
-// 				{
-// 					times.shift(); // ditch the oldest time
-// 					seconds = (times[times.length - 1] - times[0]) / 1000;
-// 					return Math.round(this.span / seconds);
-// 				} 
-// 				else return null;
-// 			},
- 
-// 			times: [],
-// 			span: 20
-// 		};
-// 		var counter = Object.create(fps_counter);
-		
-		
-		
-// 		$(document).ready( init )
-		
-// 		function init()
-// 		{
-// 			w = $(window);
-// 			container = $( '#contentContainer' );
-// 			carousel = $( '#carouselContainer' );
-// 			item = $( '.carouselItem' );
-// 			itemLength = $( '.carouselItem' ).length;
-// 			fps = $('#fps');
-// 			rY = 360 / itemLength;
-// 			radius = Math.round( (250) / Math.tan( Math.PI / itemLength ) );
-			
-// 			// set container 3d props
-// 			TweenMax.set(container, {perspective:600})
-// 			TweenMax.set(carousel, {z:-(radius)})
-			
-// 			// create carousel item props
-			
-// 			for ( var i = 0; i < itemLength; i++ )
-// 			{
-// 				var $item = item.eq(i);
-// 				var $block = $item.find('.carouselItemInner');
-				
-//         //thanks @chrisgannon!        
-//         TweenMax.set($item, {rotationY:rY * i, z:radius, transformOrigin:"50% 50% " + -radius + "px"});
-				
-// 				animateIn( $item, $block )						
-// 			}
-			
-// 			// set mouse x and y props and looper ticker
-// 			window.addEventListener( "mousemove", onMouseMove, false );
-// 			ticker = setInterval( looper, 1000/60 );			
-// 		}
-		
-// 		function animateIn( $item, $block )
-// 		{
-// 			var $nrX = 360 * getRandomInt(2);
-// 			var $nrY = 360 * getRandomInt(2);
-				
-// 			var $nx = -(2000) + getRandomInt( 4000 )
-// 			var $ny = -(2000) + getRandomInt( 4000 )
-// 			var $nz = -4000 +  getRandomInt( 4000 )
-				
-// 			var $s = 1.5 + (getRandomInt( 10 ) * .1)
-// 			var $d = 1 - (getRandomInt( 8 ) * .1)
-			
-// 			TweenMax.set( $item, { autoAlpha:1, delay:$d } )	
-// 			TweenMax.set( $block, { z:$nz, rotationY:$nrY, rotationX:$nrX, x:$nx, y:$ny, autoAlpha:0} )
-// 			TweenMax.to( $block, $s, { delay:$d, rotationY:0, rotationX:0, z:0,  ease:Expo.easeInOut} )
-// 			TweenMax.to( $block, $s-.5, { delay:$d, x:0, y:0, autoAlpha:1, ease:Expo.easeInOut} )
-// 		}
-		
-// 		function onMouseMove(event)
-// 		{
-// 			mouseX = -(-(window.innerWidth * .5) + event.pageX) * .0025;
-// 			mouseY = -(-(window.innerHeight * .5) + event.pageY ) * .01;
-// 			mouseZ = -(radius) - (Math.abs(-(window.innerHeight * .5) + event.pageY ) - 200);
-// 		}
-		
-// 		// loops and sets the carousel 3d properties
-// 		function looper()
-// 		{
-// 			addX += mouseX
-// 			TweenMax.to( carousel, 1, { rotationY:addX, rotationX:mouseY, ease:Quint.easeOut } )
-// 			TweenMax.set( carousel, {z:mouseZ } )
-// 			fps.text( 'Framerate: ' + counter.tick() + '/60 FPS' )	
-// 		}
-		
-// 		function getRandomInt( $n )
-// 		{
-// 			return Math.floor((Math.random()*$n)+1);	
-// 		}
-// }, [])
+  useEffect(() => {
+   // You can change global variables here:
+    var radius = 240; // how big of the radius
+    var autoRotate = true; // auto rotate or not
+    var rotateSpeed = -60; // unit: seconds/360 degrees
+    var imgWidth = 120; // width of images (unit: px)
+    var imgHeight = 170; // height of images (unit: px)
 
+    var bgMusicURL =
+      "https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a";
+    var bgMusicControls = true; // Show UI music control
 
-//   const [mouseX, setMouseX] = useState(0);
-//   const [mouseY, setMouseY] = useState(0);
-//   const [mouseZ, setMouseZ] = useState(0);
-//   const [addX, setAddX] = useState(0);
+    setTimeout(init, 1000);
 
-//   useEffect(() => {
-//     const w = window;
-//     const container = document.getElementById("contentContainer");
-//     const carousel = document.getElementById("carouselContainer");
-//     const items = document.querySelectorAll(".carouselItem");
-//     const itemLength = items.length;
-//     let radius = 0;
+    var odrag = dragContainer.current;
+    var ospin = spinContainer.current;
+    var aImg = ospin.getElementsByTagName("img");
+    var aVid = ospin.getElementsByTagName("video");
+    var aEle = [...aImg, ...aVid]; // combine 2 arrays
 
-//     const init = () => {
-//       const rY = 360 / itemLength;
-//       radius = Math.round(250 / Math.tan(Math.PI / itemLength));
+    // Size of images
+    ospin.style.width = imgWidth + "px";
+    ospin.style.height = imgHeight + "px";
 
-//       gsap.set(container, { perspective: 600 });
-//       gsap.set(carousel, { z: -radius });
+    // Size of ground - depend on radius
+    var ground = groundRef.current
+    ground.style.width = radius * 3 + "px";
+    ground.style.height = radius * 3 + "px";
 
-//       items.forEach((item, i) => {
-//         const block = item.querySelector(".carouselItemInner");
+    function init(delayTime) {
+      for (var i = 0; i < aEle.length; i++) {
+        aEle[i].style.transform =
+          "rotateY(" +
+          i * (360 / aEle.length) +
+          "deg) translateZ(" +
+          radius +
+          "px)";
+        aEle[i].style.transition = "transform 1s";
+        aEle[i].style.transitionDelay =
+          delayTime || (aEle.length - i) / 4 + "s";
+      }
+    }
 
-//         gsap.set(item, {
-//           rotationY: rY * i,
-//           z: radius,
-//           transformOrigin: `50% 50% ${-radius}px`,
-//         });
+    function applyTranform(obj) {
+      // Constrain the angle of camera (between 0 and 180)
+      if (tY > 180) tY = 180;
+      if (tY < 0) tY = 0;
 
-//         animateIn(item, block);
-//       });
+      // Apply the angle
+      obj.style.transform = "rotateX(" + -tY + "deg) rotateY(" + tX + "deg)";
+    }
 
-//       w.addEventListener("mousemove", onMouseMove, false);
-//       setInterval(looper, 1000 / 60);
-//     };
+    function playSpin(yes) {
+      ospin.style.animationPlayState = yes ? "running" : "paused";
+    }
 
-//     const animateIn = (item, block) => {
-//       const nrX = 360 * getRandomInt(2);
-//       const nrY = 360 * getRandomInt(2);
-//       const nx = -2000 + getRandomInt(4000);
-//       const ny = -2000 + getRandomInt(4000);
-//       const nz = -4000 + getRandomInt(4000);
-//       const s = 1.5 + getRandomInt(10) * 0.1;
-//       const d = 1 - getRandomInt(8) * 0.1;
+    var sX,
+      sY,
+      nX,
+      nY,
+      desX = 0,
+      desY = 0,
+      tX = 0,
+      tY = 10;
 
-//       gsap.set(item, { autoAlpha: 1, delay: d });
-//       gsap.set(block, {
-//         z: nz,
-//         rotationY: nrY,
-//         rotationX: nrX,
-//         x: nx,
-//         y: ny,
-//         autoAlpha: 0,
-//       });
-//       gsap.to(block, s, {
-//         delay: d,
-//         rotationY: 0,
-//         rotationX: 0,
-//         z: 0,
-//         ease: Expo.easeInOut,
-//       });
-//       gsap.to(block, s - 0.5, {
-//         delay: d,
-//         x: 0,
-//         y: 0,
-//         autoAlpha: 1,
-//         ease: Expo.easeInOut,
-//       });
-//     };
+    // auto spin
+    if (autoRotate) {
+      var animationName = rotateSpeed > 0 ? "spin" : "spinRevert";
+      ospin.style.animation = `${animationName} ${Math.abs(
+        rotateSpeed
+      )}s infinite linear`;
+    }
 
-//     const onMouseMove = (event) => {
-//       setMouseX(-(w.innerWidth * 0.5 - event.pageX) * 0.0025);
-//       setMouseY(-(w.innerHeight * 0.5 - event.pageY) * 0.01);
-//       setMouseZ(-radius - (Math.abs(w.innerHeight * 0.5 - event.pageY) - 200));
-//     };
+    // add background music
+    if (bgMusicURL) {
+      musicContainer.innerHTML += `
+    <audio src="${bgMusicURL}" ${
+        bgMusicControls ? "controls" : ""
+      } autoplay loop>    
+    <p>If you are reading this, it is because your browser does not support the audio element.</p>
+    </audio>
+    `;
+    }
 
-//     const looper = () => {
-//       setAddX((prevAddX) => prevAddX + mouseX);
-//       gsap.to(carousel, 1, {
-//         rotationY: addX,
-//         rotationX: mouseY,
-//         ease: Quint.easeOut,
-//       });
-//       gsap.set(carousel, { z: mouseZ });
-//       // Assuming you have a state variable to store the framerate
-//       // setFps(`Framerate: ${counter.tick()}/60 FPS`);
-//     };
+    // setup events
+    document.onpointerdown = function (e) {
+      clearInterval(odrag.timer);
+      e = e || window.event;
+      var sX = e.clientX,
+        sY = e.clientY;
 
-//     const getRandomInt = (n) => {
-//       return Math.floor(Math.random() * n + 1);
-//     };
+      this.onpointermove = function (e) {
+        e = e || window.event;
+        var nX = e.clientX,
+          nY = e.clientY;
+        desX = nX - sX;
+        desY = nY - sY;
+        tX += desX * 0.1;
+        tY += desY * 0.1;
+        applyTranform(odrag);
+        sX = nX;
+        sY = nY;
+      };
 
-//     init();
+      this.onpointerup = function (e) {
+        odrag.timer = setInterval(function () {
+          desX *= 0.95;
+          desY *= 0.95;
+          tX += desX * 0.1;
+          tY += desY * 0.1;
+          applyTranform(odrag);
+          playSpin(false);
+          if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+            clearInterval(odrag.timer);
+            playSpin(true);
+          }
+        }, 17);
+        this.onpointermove = this.onpointerup = null;
+      };
 
-//     // Cleanup function
-//     return () => {
-//       window.removeEventListener("mousemove", onMouseMove);
-//       clearInterval(looper);
-//     };
-//   }, [addX, mouseX, mouseY, mouseZ]);
+      return false;
+    };
+
+    document.onmousewheel = function (e) {
+      e = e || window.event;
+      var d = e.wheelDelta / 20 || -e.detail;
+      radius += d;
+      init(1);
+    };
+  }, []);
 
   return (
-    <div ref={contentContainer} id="contentContainer" className={cx(styles.contentContainer, styles.trans3d)}>
-      <section
-        ref={carouselContainer}
-        id="carouselContainer"
-        className={cx(styles.carouselContainer, styles.trans3d)}
-      >
-        <figure
-          ref={carouselItem}
-          id="item1"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
+    <>
+      <div ref={dragContainer} className={styles["drag-container"]}>
+        <div ref={spinContainer} className={styles["spin-container"]}>
+          <Image src={ImageTest} alt="" />
+          <Image src={ImageTest} alt="" />
+          <Image src={ImageTest} alt="" />
+          <Image src={ImageTest} alt="" />
+          <Image src={ImageTest} alt="" />
+          <Image src={ImageTest} alt="" />
+
+          <a
+            target="_blank"
+            href="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg"
           >
-            1
-          </div>
-        </figure>
-        <figure
-          ref={carouselItem}
-          id="item2"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
-          >
-            2
-          </div>
-        </figure>
-        <figure
-          ref={carouselItem}
-          id="item3"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
-          >
-            3
-          </div>
-        </figure>
-        <figure
-          ref={carouselItem}
-          id="item4"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
-          >
-            4
-          </div>
-        </figure>
-        <figure
-          ref={carouselItem}
-          id="item5"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
-          >
-            5
-          </div>
-        </figure>
-        <figure
-          ref={carouselItem}
-          id="item6"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
-          >
-            6
-          </div>
-        </figure>
-        <figure
-          ref={carouselItem}
-          id="item7"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
-          >
-            7
-          </div>
-        </figure>
-        <figure
-          ref={carouselItem}
-          id="item8"
-          className={cx(styles.carouselItem, styles.trans3d)}
-        >
-          <div
-            ref={carouselItemInner}
-            className={cx(styles.carouselItemInner, styles.trans3d)}
-          >
-            8
-          </div>
-        </figure>
-      </section>
-    </div>
+            <Image src={ImageTest} alt="" />
+          </a>
+        </div>
+        <div ref={groundRef} className={styles["ground"]}></div>
+      </div>
+      <div ref={musicContainer} className={styles["music-container"]} id=""></div>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,6 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        // 'hero1':"url('/assets/images/hero1.jpg')",
-        // 'hero2':"url('/assets/images/hero2.jpg')",
-        // 'hero3':"url('/assets/images/hero3.jpg')",
-        // 'hero4':"url('/assets/images/hero4.jpg')",
-        // 'hero5':"url('/assets/images/hero5.jpg')",
       },
     },
   },
